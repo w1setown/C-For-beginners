308f5eb15a2f35889be009be305606cd823145ae
Gabriel Ganderup <gabrielsogaard@gmail.com>
2025-07-01 14:44:49 +0200

    Add Challenge_2 project with initial files
    Introduces the Challenge_2 project, including configuration files (.clang-format, .clangd, and CodeLite project), a starter main.c with requirements as comments, and updates the workspace to include and activate Challenge_2.

---
+++ Projects/Challenge_2/main.c
@@ -0,0 +1,37 @@
+/*
+Requirements:
+ *  In this challenge, you must create a C program that defines 
+    an enum type and uses the values of some variables
+   
+ * This program should create an enum type named Company
+   * Valid values for this type are GOOGLE, FACEBOOK,
+     XEROX, YAHOO, EBAY, MICROSOFT
+
+ * The program should create three variables of the above enum
+   type that are assigned values: XEROX, GOOGLE AND EBAY
+
+ * The program should display output, the value of the three 
+   variables with each variable spearated by a newline.
+    * Correct output would be if XEROX, GOOGLE AND EBAY var-
+      iables are printed in that order:
+       * 2
+       * 0
+       * 4
+
+Hints
+ * Define the enum type and its values
+
+ * Declare and initialize three variables with values speci-
+   fied in the requirements
+
+ * Use printf to diaplay the value of the enum variables
+    * Use the '\n' escape character to display new line  
+*/
+
+
+
+#include <stdio.h>
+
+int main(){
+
+}
